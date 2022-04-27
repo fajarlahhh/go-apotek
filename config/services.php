597e6f34c4217b9239730d2caa8d949ed87dd88f
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-04-27 13:53:52 +0800

    baru

---
+++ config/services.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'facebook' => [
+        'client_id' => 'Facebook app id',
+        'client_secret' => 'Facebook add secret',
+        'redirect' => 'http://localhost:8000/auth/facebook/callback',
+    ],
+
+    'google' => [
+        'client_id' => '486193528861-c8bsmd72brirhih1i9q1pjro16o2vsmf.apps.googleusercontent.com',
+        'client_secret' => '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',
+        'redirect' => 'http://localhost:8080/auth/google/callback',
+    ],
+];
